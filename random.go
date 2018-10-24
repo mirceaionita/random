10cce41d10cf0e5b7b33f3137f024a25a2186aaf
Mircea Ionita <mionita@deloittece.com>
2018-10-24 12:32:31 +0300

    created funcs: RandString and RandStringLen

---
+++ random.go
@@ -0,0 +1,19 @@
+package service
+
+import "math/rand"
+
+const DEFAULT_SIZE = 12
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandString() string {
+	return RandStringLen(DEFAULT_SIZE)
+}
+
+func RandStringLen(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
